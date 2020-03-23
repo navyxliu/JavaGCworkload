890a191aea6f355c07586a9815ecac0ae62cc346
Xin Liu <xxinliu@amazon.com>
2020-03-23 07:38:01 +0000

    add infinite mode(-1)

--- runJava.sh
+++ runJava.sh
@@ -8,4 +8,4 @@ GCLOG=$1
 shift
 
 #10g heap is suggested
-java -Xmx$H -Xms$H -Xloggc:$GCLOG -XX:+UseG1GC -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCDetails $* Test
+java -Xmx$H -Xms$H -Xloggc:$GCLOG -XX:+UseG1GC -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCDetails $* Test -1 4
