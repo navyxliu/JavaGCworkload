cbf373eaacd0d752127e7f274ab2f2c88e3219f8
Xin Liu <xxinliu@amazon.com>
2021-04-22 06:00:45 +0000

    Dump all=trace and display dropped log messages.

--- run-and-compare.sh
+++ run-and-compare.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+echo "testing: " `which java`
 #expr: https://openjdk.java.net/jeps/158
-WHAT='gc*,safepoint*,class*=trace,thread*=trace,jit*=debug'
+#WHAT='gc*,safepoint*,class*=trace,thread*=trace,jit*=debug'
+WHAT='all=trace'
 DECORATORS='l,tg'
-java -Xlog:"$WHAT:file=gc.log:$DECORATORS:filecount=1,async=true" --version 
-java -Xlog:"$WHAT:file=gc-golden.log:$DECORATORS:filecount=1,async=false" --version 
-wc -l gc.log gc-golden.log
+time java -Xlog:"$WHAT:file=hotspot.log:$DECORATORS:filecount=0,async=true" $* --version
+time java -Xlog:"$WHAT:file=hotspot-gold.log:$DECORATORS:filecount=0,async=false" $* --version
+wc -l hotspot.log hotspot-gold.log
+
+echo "dropped logs in async mode:"
+grep "messages dropped" hotspot.log
